8c78bf7c37d9b7088b229609e1c5403c5a68b12b
carcharo <barryswhite@gmail.com>
2018-12-08 19:17:35 +0000

    update

---
+++ src/chip8.rs
@@ -0,0 +1,21 @@
+use memory::Memory;
+
+pub struct Chip8 {
+    memory: Memory,
+}
+
+impl Chip8{
+    pub fn new() -> Chip8 {
+        Chip8 {
+            memory: Memory::new()
+        }
+    }
+
+    pub fn load_rom(&mut self, data: &Vec<u8>) {
+        let offset = 0x200;
+        for i in 0..data.len() {
+            self.memory.write_byte((offset + i) as u16, data[i]);
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 
 use std::io::prelude::*;
 use std::fs::File;
+use chip8::Chip8;
 
+mod memory;
+mod chip8;
 
 fn main() {
     let mut file = File::open("data/INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
-    file.read_to_end(&mut data);
+    file.read_to_end(&mut data).expect("could not read file");
 
-    
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(data);
 
 }
