1741baf64fb2b2c55ea5375a48f41b30ba964c4f
carcharo <barryswhite@gmail.com>
2018-12-08 18:19:38 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+use std::io::prelude::*;
+use std::fs::File;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("data/INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+
+    
+
 }
